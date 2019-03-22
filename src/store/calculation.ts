272095c55156ca4bdb34ab41a8c88162b20c267d
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2019-03-22 17:51:54 -0300

    Bootstraping project

---
+++ src/store/calculation.ts
@@ -0,0 +1,27 @@
+import { Module } from "vuex";
+
+import { Sign } from "@/types/sign";
+
+interface ICalculationState {
+  left: number;
+  right: number;
+  sign: Sign;
+}
+
+const SET_SIGN = "SET_SIGN";
+
+const calculation: Module<ICalculationState, {}> = {
+  namespaced: true,
+  state: {
+    left: 3,
+    right: 1,
+    sign: Sign["+"]
+  },
+  mutations: {
+    [SET_SIGN](state, payload: Sign) {
+      state.sign = payload;
+    }
+  }
+};
+
+export { calculation, ICalculationState };
---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+import { calculation, ICalculationState } from "./calculation";
+
+Vue.use(Vuex);
+
+interface IState {
+  calculation: ICalculationState;
+}
+
+const store = new Vuex.Store<IState>({
+  modules: {
+    calculation
+  }
+});
+
+export { store, IState };
